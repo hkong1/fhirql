43f6b5922d3e01e112beeef29f6a247bcf0715e5
hkong1 <64344936+hkong1@users.noreply.github.com>
2020-10-22 09:53:49 -0500

    Initialize interceptor

---
+++ src/main/java/com/canehealth/interceptor/DataElementResponseInterceptor.java
@@ -0,0 +1,26 @@
+package com.canehealth.interceptor;
+
+import ca.uhn.fhir.rest.api.server.RequestDetails;
+import ca.uhn.fhir.rest.server.interceptor.InterceptorAdapter;
+import org.hl7.fhir.dstu3.model.Questionnaire;
+import org.hl7.fhir.dstu3.model.Resource;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class DataElementResponseInterceptor extends InterceptorAdapter {
+
+    @Autowired
+    private DataElementInterceptor dataElementInterceptor;
+
+    @Override
+    public boolean outgoingResponse(RequestDetails theRequestDetails, IBaseResource theResponseObject) {
+        if (theResponseObject instanceof Resource) {
+            if (theResponseObject.getClass() == Questionnaire.class) {
+                Questionnaire questionnaire = (Questionnaire) theResponseObject;
+                theResponseObject = dataElementInterceptor.inject(questionnaire);
+            }
+        }
+
+        return true;
+    }
+}
